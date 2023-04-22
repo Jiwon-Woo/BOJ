7ef588233db86be63e39acfa7537991a968e02ff
jwoo <74581396+Jiwon-Woo@users.noreply.github.com>
2023-04-22 15:48:40 +0900

    [Silver II] Title: 에디터, Time: 216 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,82 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <list>
-
-using namespace std;
-
-int main() {
-	string s, command, c;
-	int m;
-
-	cin >> s;
-	scanf("%d", &m);
-
-	list<char> text;
-	for (int i = 0; i < s.size(); i++) {
-		text.push_back(s[i]);
-	}
-	list<char>::iterator cursor = text.end();
-
-	for (int i = 0; i < m; i++) {
-		cin >> command;
-		if (command == "P") {
-			cin >> c;
-		}
-
-		// cout << "before: ";
-		// list<char>::iterator it;
-		// for (it = text.begin(); it != text.end(); it++) {
-		// 	if (it == cursor) {
-		// 		cout <<"|";
-		// 	}
-		// 	cout << *it;
-		// }
-		// if (it == cursor) {
-		// 	cout <<"|";
-		// }
-		// cout << "\n";
-
-		switch (command[0])
-		{
-		case 'L':
-			if (cursor != text.begin()) {
-				cursor--;
-			}
-			break;
-		case 'D':
-			if (cursor != text.end()) {
-				cursor++;
-			}
-			break;
-		case 'B':
-			if (cursor != text.begin()) {
-				cursor = text.erase(--cursor);
-			}
-			break;
-		case 'P':
-			text.insert(cursor, c[0]);
-			// cursor++;
-			break;
-		default:
-			break;
-		}
-
-		// cout << "after: ";
-		// for (it = text.begin(); it != text.end(); it++) {
-		// 	if (it == cursor) {
-		// 		cout <<"|";
-		// 	}
-		// 	cout << *it;
-		// }
-		// if (it == cursor) {
-		// 	cout <<"|";
-		// }
-		// cout << "\n";
-	}
-
-	for (list<char>::iterator it = text.begin(); it != text.end(); it++) {
-		cout << *it;
-	}
-
-	return 0;
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
