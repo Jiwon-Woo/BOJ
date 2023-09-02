403994a3cc6e7552dfdf182f56d65be3a9856629
jwoo <74581396+Jiwon-Woo@users.noreply.github.com>
2023-09-03 02:31:34 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -2,23 +2,14 @@ import sys
 
 N = int(sys.stdin.readline())
 interviews = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp = [-1 for _ in range(N + 1)]
+dp = [0 for _ in range(N + 1)]
 
 
-def recur(index):
-    global interviews
-
-    if index > N:
-        return -15001
-    if index == N:
-        return 0
-    if dp[index] > -1:
-        return dp[index]
-
+for index in range(N)[::-1]:
     ti, pi = interviews[index]
-    dp[index] = max(recur(index + ti) + pi, recur(index + 1))
-    return dp[index]
+    if index + ti > N:
+        dp[index] = dp[index + 1]
+    else:
+        dp[index] = max(dp[index + ti] + pi, dp[index + 1])
 
-
-recur(0)
 print(dp[0])
